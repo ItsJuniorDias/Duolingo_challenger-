ff88a1072f5b757e678ac618628d1778d2f021d8
AlexandreJr <juniordias_@live.com>
2024-11-03 14:50:39 -0300

    chore: create section notebook

--- src/components/Why/Why.tsx
+++ src/components/Why/Why.tsx
@@ -1,10 +1,25 @@
 import React from "react";
 
+import fireIcon from "../../assets/fireIcon.svg";
+import fireCheck from "../../assets/iconCheck.svg";
+import fireNotebook from "../../assets/iconNotebook.svg";
+import iconKing from "../../assets/icon_king.svg";
+import iconGirl from "../../assets/icon_girl.svg";
+
 import {
   ContainerWhy,
   ContentWhy,
   ContainerNotebook,
   TitleWhy,
+  Column,
+  Row,
+  Description,
+  Text,
+  ContainerIcon,
+  Img,
+  ContainerStay,
+  ImageNotebookg,
+  ImgHave,
 } from "./styles";
 
 export const Why = () => {
@@ -13,7 +28,73 @@ export const Why = () => {
       <ContentWhy>
         <TitleWhy>{`Why you’ll love learning with Duolingo`}</TitleWhy>
 
-        <ContainerNotebook></ContainerNotebook>
+        <ContainerNotebook>
+          <Row>
+            <ContainerIcon>
+              <Img src={fireIcon} />
+            </ContainerIcon>
+            <Column>
+              <Text>Effective and efficient</Text>
+
+              <Description>
+                Our courses effectively and efficiently teach reading,
+                listening, and speaking skills. Check out our latest research!
+              </Description>
+            </Column>
+
+            <ContainerStay>
+              <ImageNotebookg src={fireNotebook} />
+            </ContainerStay>
+
+            <ContainerIcon>
+              <Img src={iconKing} />
+            </ContainerIcon>
+            <Column>
+              <Text>Stay motivated</Text>
+
+              <Description>
+                We make it easy to form a habit of language learning, with
+                game-like features, fun challenges, and reminders from our
+                friendly mascot, Duo the owl.
+              </Description>
+            </Column>
+          </Row>
+        </ContainerNotebook>
+
+        <ContainerNotebook>
+          <Row>
+            <ContainerIcon>
+              <Img src={fireCheck} />
+            </ContainerIcon>
+            <Column>
+              <Text>Personalized learning</Text>
+
+              <Description>
+                Combining the best of AI and language science, lessons are
+                tailored to help you learn at just the right level and pace.
+              </Description>
+            </Column>
+
+            <ContainerIcon>{/* <Img src={iconKing} /> */}</ContainerIcon>
+            <Column>
+              <Text></Text>
+
+              <Description></Description>
+            </Column>
+
+            <ContainerIcon>
+              <ImgHave src={iconGirl} />
+            </ContainerIcon>
+            <Column>
+              <Text>Have fun with it!</Text>
+
+              <Description>
+                Effective learning doesn’t have to be boring! Build your skills
+                each day with engaging exercises and playful characters.
+              </Description>
+            </Column>
+          </Row>
+        </ContainerNotebook>
       </ContentWhy>
     </ContainerWhy>
   );
--- src/components/Why/styles.ts
+++ src/components/Why/styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ContainerWhy = styled.div`
   width: 100%;
   height: 475px;
-  background-color: red;
+  /* background-color: red; */
   display: flex;
   justify-content: center;
   align-items: center;
@@ -12,7 +12,7 @@ export const ContainerWhy = styled.div`
 export const ContentWhy = styled.div`
   width: 1065px;
   height: 475px;
-  background-color: blue;
+  /* background-color: blue; */
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -28,5 +28,72 @@ export const TitleWhy = styled.h5`
 export const ContainerNotebook = styled.div`
   width: 100%;
   height: 344px;
-  background-color: green;
+  /* background-color: green; */
+`;
+
+export const Column = styled.div`
+  width: 322px;
+  height: 148px;
+  display: flex;
+  /* padding-left: 32px; */
+  flex-direction: column;
+  /* background-color: yellow; */
+  margin-right: 32px;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  /* background-color: pink; */
+`;
+
+export const Text = styled.h2`
+  font-family: "Nunito-Bold";
+  font-size: 16px;
+  color: #3c3c3c;
+`;
+
+export const Description = styled.h5`
+  font-family: "Nunito-Bold";
+  font-size: 14px;
+  color: #777777;
+  margin-top: -8px;
+`;
+
+export const ContainerIcon = styled.div`
+  height: 43px;
+  width: 5%;
+  /* background-color: gray; */
+  z-index: 1;
+  margin-top: 8px;
+  /* padding-top: 32px; */
+`;
+
+export const Img = styled.img`
+  width: 42px;
+  height: 42px;
+`;
+
+export const ImgHave = styled.img`
+  width: 42px;
+  height: 42px;
+  /* background-color: red; */
+  margin-right: -500px;
+`;
+
+export const ContainerStay = styled.div`
+  width: 322px;
+  height: 148px;
+  /* background-color: gray; */
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin-top: 32px;
+`;
+
+export const ImageNotebookg = styled.img`
+  width: 245px;
+  height: 161px;
+  margin-bottom: -64px;
 `;
