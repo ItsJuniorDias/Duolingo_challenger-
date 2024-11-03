b5b671404e6f9b15bdbc1fbc0f88a5438c7b1e1b
AlexandreJr <juniordias_@live.com>
2024-11-03 16:26:55 -0300

    chore:adjust style

--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -57,34 +57,3 @@ export const ContentDivider = styled.div`
   display: flex;
   justify-content: center;
 `;
-
-export const ContainerWhy = styled.div`
-  width: 100%;
-  height: 475px;
-  background-color: red;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const ContentWhy = styled.div`
-  width: 1065px;
-  height: 475px;
-  background-color: blue;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const TitleWhy = styled.h5`
-  font-family: "Nunito-Bold";
-  font-size: 22px;
-  color: #3c3c3c;
-`;
-
-export const ContainerNotebook = styled.div`
-  width: 100%;
-  height: 344px;
-  background-color: green;
-`;
